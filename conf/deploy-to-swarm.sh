c87ec2cb2b0410668a889d2ae63e29bfe4380c3d
Pietrino Atzeni <pietrino.atzeni@moneyfarm.com>
2018-01-18 10:32:16 +0100

    Added conf

---
+++ conf/deploy-to-swarm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -xeu
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${SWARM_USERNAME}@${SWARM_URL} << EOF
+DOCKER_LOGIN=$(aws ecr get-login --no-include-email --region eu-west-1) && ${DOCKER_LOGIN}
+docker service rm ${SERVICE_NAME}
+docker service create ${SERVICE_EXTRA_CONFIG} --name ${SERVICE_NAME} --detach=false --with-registry-auth ${SERVICE_REPOSITORY}/${SERVICE_NAME}:${SERVICE_TAG}
+docker service ps ${SERVICE_NAME}
+EOF
