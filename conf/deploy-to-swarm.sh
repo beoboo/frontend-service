b83ea7aed76fe4253a5c12e2a40926a623ef6ef7
Pietrino Atzeni <pietrino.atzeni@moneyfarm.com>
2018-01-18 16:55:03 +0100

    Added ping and curl

--- conf/deploy-to-swarm.sh
+++ conf/deploy-to-swarm.sh
@@ -2,10 +2,10 @@
 
 set -xeu
 
-#ping ${SWARM_URL}
-curl ${SWARM_URL}:2375/images/json
+#ping ${SWARM_URL} -t 10
+#curl ${SWARM_URL}:2375/images/json
 
-ssh -vvvv -i /root/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${SWARM_USERNAME}@${SWARM_URL} /bin/bash << EOF
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${SWARM_USERNAME}@${SWARM_URL} /bin/bash << EOF
 DOCKER_LOGIN=\$(aws ecr get-login --no-include-email --region eu-west-1) && \$DOCKER_LOGIN
 docker service rm ${SERVICE_NAME}
 docker service create ${SERVICE_EXTRA_CONFIG} --name ${SERVICE_NAME} --detach=false --with-registry-auth ${SERVICE_REPOSITORY}/${SERVICE_NAME}:${SERVICE_TAG}
